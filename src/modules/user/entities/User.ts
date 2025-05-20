0f6939387e74e5cc54e1955fd25cd511774a8e19
Henrique Marques <henrique.marquesvj@gmail.com>
2025-05-19 23:39:08 -0300

    criando gets e sets

--- src/modules/user/entities/User.ts
+++ src/modules/user/entities/User.ts
@@ -1,5 +1,5 @@
 import { randomUUID } from "crypto";
-
+import { Replace } from "src/utils/replace";
 interface UserSchema{
     email : String;
     password : String;
@@ -12,8 +12,35 @@ export class User{
     props: UserSchema;
     _id: String;
 
-    constructor(props: UserSchema, id?: string){
-        this.props = props;
+    constructor(props: Replace<UserSchema,{ createdAt?: Date }>, id?: String){
+        this.props = {
+            ...props,
+            createdAt: props.createdAt || new Date(),
+        };
         this._id = id || randomUUID();
+    }
+    get id(): String{
+        return this._id;
+    }
+    get email(): String{
+        return this.props.email;
+    }
+    set email(email: string){
+        this.props.email = email;
+    }
+    get password(): String{
+        return this.props.password;
+    }
+    set password(password: String){
+        this.props.password = password;
+    }
+    get name(): String{
+        return this.props.name;
+    }
+    set name(name: String){
+        this.props.name = name;
+    }
+    get createdAt(): String{
+        return this.createdAt;
     }
 }
